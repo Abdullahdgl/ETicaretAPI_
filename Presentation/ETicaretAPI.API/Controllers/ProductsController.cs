8acf9fae119ed56066e7aa644273bec17f35505e
Abdullah Dagli <abdullah.dagli@outlook.com>
2025-01-28 06:19:07 +0300

    Cors policy 2

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -11,102 +11,31 @@ namespace ETicaretAPI.API.Controllers
 	[ApiController]
 	public class ProductsController : ControllerBase
 	{
-		#region Önceki örneklem!!!!
-		//private readonly IProductService _productService;
-
-		//public ProductsController(IProductService productService)
-		//{
-		//	_productService = productService;
-		//}
-		//[HttpGet]
-		//public IActionResult GetProducts()
-		//{
-		//	var products = _productService.GetProducts();
-		//	return Ok(products);
-		//}
-		#endregion
+	
 
 		readonly private IProductReadRepository _productReadRepository;
 		readonly private IProductWriteRepository _productWriteRepository;
 
-		readonly private IOrderWriteRepository _orderWriteRepository;
-		readonly private IOrderReadRepository _orderReadRepository;
+	
 
-
-		readonly private ICustomerWriteRepository _customerWriteRepository;
-		
-
-		public ProductsController(IProductWriteRepository productWriteRepository,
-			IProductReadRepository productReadRepository,
-			IOrderWriteRepository orderWriteRepository,
-			ICustomerWriteRepository customerWriteRepository,
-			IOrderReadRepository orderReadRepository)
+		public ProductsController(
+			IProductWriteRepository productWriteRepository,
+			IProductReadRepository productReadRepository)
+			
 		{
 			_productReadRepository = productReadRepository;
 			_productWriteRepository = productWriteRepository;
-			_orderWriteRepository = orderWriteRepository;
-			_customerWriteRepository = customerWriteRepository;
-			_orderReadRepository = orderReadRepository;
+			
 		}
 
 
 		[HttpGet]
 
-		public IActionResult Get()
+		public async Task<IActionResult>Get()
 		{
-			return(Ok("Merhabalar,"));
+			return (Ok("Merhabalar,"));
 		}
-		//[HttpGet]
-		//public async Task Get()
-		//{
-			#region Orneklem
-			//1.Örnek
-			//	await _productWriteRepository.AddRangeAsync(new()
-			//	{
-			//		new(){Id=Guid.NewGuid(), Name="Prouduct 1", Price=100, CreatedDate=DateTime.UtcNow,Stock=10 },
-			//		new(){Id=Guid.NewGuid(), Name="Prouduct 2", Price=200, CreatedDate=DateTime.UtcNow,Stock=20 },
-			//		new(){Id=Guid.NewGuid(), Name="Prouduct 3", Price=300, CreatedDate=DateTime.UtcNow,Stock=30 },
-			//	});
-			//var count = 	await _productWriteRepository.SaveAsync();
-
-			//2.örnek 
-			//Product p = await _productReadRepository.GetByIdAsync("d74d68c0-0fbc-4325-8652-ad1c40a53e05");
-			//p.Name = "Yeni Product 10";
-			//await _productWriteRepository.SaveAsync();
-
-			//3.Örnek
-
-
-			//await _productWriteRepository.AddAsync(new() { Name = "C Product", Price = 1.500f, Stock = 10, CreatedDate = DateTime.UtcNow });
-			//await _productWriteRepository.SaveAsync();
-
-			//4.Örnek
-
-			//var customerId = Guid.NewGuid();
-			//await _customerWriteRepository.AddAsync(new() { Id = customerId, Name = "Muiiddin" });
-
-			//await _orderWriteRepository.AddAsync(new() { Description = "BLA BLA BLA ", Address = "Malatya", CustomerId = customerId });
-			//await _orderWriteRepository.AddAsync(new() { Description = "BLA BLA BLA  2", Address = "Bartın", CustomerId = customerId });
-
-			//await _orderWriteRepository.SaveAsync();
-
-			//5.Örnek Güncelleme 
-
-			//Order order = await _orderReadRepository.GetByIdAsync("b679593b-c937-4706-bf95-362df738e625");
-			//order.Address = "Bilecik";
-			//await _orderWriteRepository.SaveAsync();
-
-
-			#endregion
-
-
-		//}
-
-		//[HttpGet("{id}")]
-		//public async Task<IActionResult> Get(string id)
-		//{
-		//	Product product = await _productReadRepository.GetByIdAsync(id);
-		//	return Ok(product);
-		//}
+		
+	
 	}
 }
